86f5adb5dc5fdaea1303178ec43220fe6b95b15a
bwapes <benwaples@gmail.com>
2020-10-30 11:33:13 -0700

    house post route

--- __tests__/houses.test.js
+++ __tests__/houses.test.js
@@ -1,5 +1,21 @@
-describe('placeholder', () => {
-  it('should pass', () => {
-    expect(true).toEqual(true);
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('House routes', () => {
+  it('should insert a house', async() => {
+    const house = {
+      address: 'fake address', 
+      images: ['an image'], 
+      price: '$33093898',
+      lotSQFT: 'lod', 
+      houseSQFT: 'little', 
+      saleStatus: 'sold', 
+      lastSold: '2017', 
+    };
+
+    return await request(app)
+      .post('/api/v1/houses')
+      .send(house)
+      .then(res => expect(res.body).toEqual({ ...house, id: expect.any(String), images: expect.any(String) }));
   });
 });
